6dccc6548989f2f6de371fb87feae09562f515d7
Tomasz_Fus <fustomek38@gmail.com>
2021-06-29 15:56:05 +0200

    dodanie obslugi klienta, poprawki

--- Projekt/Models/CustomerModel.cs
+++ Projekt/Models/CustomerModel.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Projekt.Models
 {
+    [Table("Customers")]
     public class CustomerModel
     {
+        [Key]
         public int Id { get; set; }
         [DisplayName("Imię")]
+        [Required(ErrorMessage = "Podaj imię")]
+        [MaxLength(50)]
         public string FirstName { get; set; }
         [DisplayName("Nazwisko")]
+        [Required(ErrorMessage = "Podaj nazwisko")]
+        [MaxLength(50)]
         public string LastName { get; set; }
         [DisplayName("Telefon")]
+        [Required(ErrorMessage = "Podaj telefon")]
+        [MaxLength(9)]
         public string PhoneNumber { get; set; }
 
     }
